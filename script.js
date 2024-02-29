c651b4b28f52f254ef6b7f9a4a493551c4731cf5
Mat-123 <156803363+Mat-123@users.noreply.github.com>
2024-02-29 16:42:28 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const URL_PEXELS = "https://api.pexels.com/v1/search?query=";
+
+const MY_API_KEY = "tC8jYs1h7BdijPVtnUs6nXFe5pP7p0O1wOWKnGOw0Bvlmqm2eZ3WVTa2";
+
+const newImageInCard = function (pics) {};
